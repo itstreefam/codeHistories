84c1f04dd3f6cb0befd2f2c06a84aeff9092bd91
Tri Pham <thientri399@gmail.com>
2022-06-06 00:43:32 -0500

    Uncommented tracker.commit()

--- src/extension.js
+++ src/extension.js
@@ -84,7 +84,7 @@ function activate(context) {
 						var updated = tracker.updateOutput(output.replace(
 							/[\u001b\u009b][[()#;?]*(?:[0-9]{1,4}(?:;[0-9]{0,4})*)?[0-9A-ORZcf-nqry=><]/g, ''));
 						if(updated){
-							// tracker.commit();
+							tracker.commit();
 						}
 
 						iter = 1;
