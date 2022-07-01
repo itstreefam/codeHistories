be7131626b9517a11e429d82647f3580d13627d6
TriPham2110 <thientri399@gmail.com>
2022-06-30 19:31:15 -0500

    Avoid capturing incomplete copy

--- src/extension.js
+++ src/extension.js
@@ -68,11 +68,13 @@ function activate(context) {
 								lastIndexOfCurDir = output.lastIndexOf(curDir+">") - 3;
 							}
 
-							let	finalOutput = output.substring(secondToLastIndexOfCurDir, lastIndexOfCurDir);
-							let updated = tracker.updateOutput(finalOutput);
-							if(updated){
-								console.log(finalOutput);
-								tracker.checkWebData();
+							if(similarity(eventData[1], eventData[2]) <= 0.7){
+								let	finalOutput = output.substring(secondToLastIndexOfCurDir, lastIndexOfCurDir);
+								let updated = tracker.updateOutput(finalOutput);
+								if(updated){
+									console.log(finalOutput);
+									// tracker.checkWebData();
+								}	
 							}
 						}
 
@@ -175,6 +177,48 @@ function activate(context) {
 function countOccurrences(string, word) {
 	return string.split(word).length - 1;
  }
+
+// https://stackoverflow.com/questions/10473745/compare-strings-javascript-return-of-likely
+function similarity(s1, s2) {
+	var longer = s1;
+	var shorter = s2;
+	if (s1.length < s2.length) {
+		longer = s2;
+		shorter = s1;
+	}
+	var longerLength = longer.length;
+	if (longerLength == 0) {
+		return 1.0;
+	}
+	return (longerLength - editDistance(longer, shorter)) / parseFloat(longerLength);
+}
+
+function editDistance(s1, s2) {
+	s1 = s1.toLowerCase();
+	s2 = s2.toLowerCase();
+
+	var costs = new Array();
+	for (var i = 0; i <= s1.length; i++) {
+		var lastValue = i;
+		for (var j = 0; j <= s2.length; j++) {
+		if (i == 0)
+			costs[j] = j;
+		else {
+			if (j > 0) {
+			var newValue = costs[j - 1];
+			if (s1.charAt(i - 1) != s2.charAt(j - 1))
+				newValue = Math.min(Math.min(newValue, lastValue),
+				costs[j]) + 1;
+			costs[j - 1] = lastValue;
+			lastValue = newValue;
+			}
+		}
+		}
+		if (i > 0)
+		costs[s2.length] = lastValue;
+	}
+	return costs[s2.length];
+}
 
 // this method is called when your extension is deactivated
 function deactivate() {
