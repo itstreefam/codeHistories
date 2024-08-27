5fa25bc030e251ff23bebcee5b24f1a730c204c4
TriPham2110 <thientri399@gmail.com>
2024-08-27 12:37:29 -0500

    Latest changes that were in terminalShellIntegration
    For some reasons the changes were not updated even though main containing changes of terminalShellIntegration was merged into integrating-live-history?

--- src/profileHelpers.js
+++ src/profileHelpers.js
@@ -15,11 +15,11 @@ codehistories() {
   # Get current date and time in the format [M/D/YYYY, HH:MM:SS AM/PM]
   timestamp=$(date +"[%-m/%-d/%Y, %I:%M:%S %p]")
   
-  # Print a newline and the timestamp to output.txt
-  echo -e "\nExecution Time: $timestamp" | tee -a output.txt
+  # Print a newline and the timestamp
+  echo -e "\nExecution Time: $timestamp"
   
-  # Execute the command and append the output
-  eval "$cmd" 2>&1 | tee -a output.txt
+  # Execute the command
+  eval "$cmd" 2>&1
 }`;
 
     ensureDirectoryExists(dirPath);
