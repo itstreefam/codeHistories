387b47ba43af0455f32ee03acde06fad90e2f5a6
Tri Pham <thientri399@gmail.com>
2025-10-22 15:40:33 -0500

    using bootstrap icons instead of svg

--- src/clusterManager.js
+++ src/clusterManager.js
@@ -1766,12 +1766,8 @@ Rules:
                             <div class="li-header">
                                 <button type="button" class="collapsible" id="plusbtn-${groupKey}-${subgoalKey}">+</button>
                                 <input class="editable-title" id="code-title-${groupKey}-${subgoalKey}" value="${subgoal.title}" onchange="updateCodeTitle('${groupKey}', '${subgoalKey}')" size="50">
-                                <!-- <i class="bi bi-pencil-square"></i> -->
                                 <button type="button" class="btn btn-secondary" id="button-${groupKey}-${subgoalKey}">
-                                    <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-pencil-square" viewBox="0 0 16 16">
-                                    <path d="M15.502 1.94a.5.5 0 0 1 0 .706L14.459 3.69l-2-2L13.502.646a.5.5 0 0 1 .707 0l1.293 1.293zm-1.75 2.456-2-2L4.939 9.21a.5.5 0 0 0-.121.196l-.805 2.414a.25.25 0 0 0 .316.316l2.414-.805a.5.5 0 0 0 .196-.12l6.813-6.814z"></path>
-                                    <path fill-rule="evenodd" d="M1 13.5A1.5 1.5 0 0 0 2.5 15h11a1.5 1.5 0 0 0 1.5-1.5v-6a.5.5 0 0 0-1 0v6a.5.5 0 0 1-.5.5h-11a.5.5 0 0 1-.5-.5v-11a.5.5 0 0 1 .5-.5H9a.5.5 0 0 0 0-1H2.5A1.5 1.5 0 0 0 1 2.5z"></path>
-                                    </svg>
+                                    <i class="bi bi-pencil-square"></i>
                                 </button>
                                 <b>in ${subgoal.file} </b> `
                     const link = links.resources[count];
@@ -1820,12 +1816,8 @@ Rules:
                             <div class="li-header">
                                 <button type="button" class="collapsible" id="plusbtn-${groupKey}-${subgoalKey}">+</button>
                                 <input class="editable-title" id="code-title-${groupKey}-${subgoalKey}" value="${subgoal.title}" onchange="updateCodeTitle('${groupKey}', '${subgoalKey}')" size="50">
-                                <!-- <i class="bi bi-pencil-square"></i> -->
                                 <button type="button" class="btn btn-secondary" id="button-${groupKey}-${subgoalKey}">
-                                    <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-pencil-square" viewBox="0 0 16 16">
-                                    <path d="M15.502 1.94a.5.5 0 0 1 0 .706L14.459 3.69l-2-2L13.502.646a.5.5 0 0 1 .707 0l1.293 1.293zm-1.75 2.456-2-2L4.939 9.21a.5.5 0 0 0-.121.196l-.805 2.414a.25.25 0 0 0 .316.316l2.414-.805a.5.5 0 0 0 .196-.12l6.813-6.814z"></path>
-                                    <path fill-rule="evenodd" d="M1 13.5A1.5 1.5 0 0 0 2.5 15h11a1.5 1.5 0 0 0 1.5-1.5v-6a.5.5 0 0 0-1 0v6a.5.5 0 0 1-.5.5h-11a.5.5 0 0 1-.5-.5v-11a.5.5 0 0 1 .5-.5H9a.5.5 0 0 0 0-1H2.5A1.5 1.5 0 0 0 1 2.5z"></path>
-                                    </svg>
+                                    <i class="bi bi-pencil-square"></i>
                                 </button>
                                 <b>in ${subgoal.file} </b>
                                 <div class="placeholder">
@@ -1925,10 +1917,7 @@ Rules:
                                     onchange="updateCodeTitle('${groupKey}', '${index}')" 
                                     size="50">
                                 <button type="button" class="btn btn-secondary" id="button-${groupKey}-${index}" style="margin-left: 8px;">
-                                    <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-pencil-square" viewBox="0 0 16 16">
-                                        <path d="M15.502 1.94a.5.5 0 0 1 0 .706L14.459 3.69l-2-2L13.502.646a.5.5 0 0 1 .707 0l1.293 1.293zm-1.75 2.456-2-2L4.939 9.21a.5.5 0 0 0-.121.196l-.805 2.414a.25.25 0 0 0 .316.316l2.414-.805a.5.5 0 0 0 .196-.12l6.813-6.814z"></path>
-                                        <path fill-rule="evenodd" d="M1 13.5A1.5 1.5 0 0 0 2.5 15h11a1.5 1.5 0 0 0 1.5-1.5v-6a.5.5 0 0 0-1 0v6a.5.5 0 0 1-.5.5h-11a.5.5 0 0 1-.5-.5v-11a.5.5 0 0 1 .5-.5H9a.5.5 0 0 0 0-1H2.5A1.5 1.5 0 0 0 1 2.5z"></path>
-                                    </svg>
+                                    <i class="bi bi-pencil-square"></i>
                                 </button>
                                 <b style="margin-left: 8px;">in ${event.file}</b>
                                 <div style="flex-grow: 1;"></div>
@@ -2300,10 +2289,7 @@ Rules:
                                             onchange="updateCodeTitle('${groupKey}', '${index}')" 
                                             size="50">
                                         <button type="button" class="btn btn-secondary" id="button-${groupKey}-${index}">
-                                            <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-pencil-square" viewBox="0 0 16 16">
-                                                <path d="M15.502 1.94a.5.5 0 0 1 0 .706L14.459 3.69l-2-2L13.502.646a.5.5 0 0 1 .707 0l1.293 1.293zm-1.75 2.456-2-2L4.939 9.21a.5.5 0 0 0-.121.196l-.805 2.414a.25.25 0 0 0 .316.316l2.414-.805a.5.5 0 0 0 .196-.12l6.813-6.814z"></path>
-                                                <path fill-rule="evenodd" d="M1 13.5A1.5 1.5 0 0 0 2.5 15h11a1.5 1.5 0 0 0 1.5-1.5v-6a.5.5 0 0 0-1 0v6a.5.5 0 0 1-.5.5h-11a.5.5 0 0 1-.5-.5v-11a.5.5 0 0 1 .5-.5H9a.5.5 0 0 0 0-1H2.5A1.5 1.5 0 0 0 1 2.5z"></path>
-                                            </svg>
+                                            <i class="bi bi-pencil-square"></i>
                                         </button>
                                         <b>in ${event.file} </b>
                                         ${resourcesExist ? `
@@ -2549,15 +2535,7 @@ Rules:
                                    value="${subgoal.title}" 
                                    onchange="updateCodeTitle('${groupKey}', '${subgoalKey}')" size="50">
                             <button type="button" class="btn btn-secondary" id="button-${groupKey}-${subgoalKey}">
-                                <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" 
-                                     fill="currentColor" class="bi bi-pencil-square" viewBox="0 0 16 16">
-                                    <path d="M15.502 1.94a.5.5 0 0 1 0 .706L14.459 3.69l-2-2L13.502.646a.5.5 0 0 1 .707 0l1.293 1.293z"/>
-                                    <path fill-rule="evenodd" 
-                                          d="M1 13.5A1.5 1.5 0 0 0 2.5 15h11a1.5 1.5 0 0 0 1.5-1.5v-6a.5.5 
-                                          0 0 1-1 0v6a.5.5 0 0 1-.5.5h-11a.5.5 
-                                          0 0 1-.5-.5v-11a.5.5 0 0 1 .5-.5H9a.5.5 
-                                          0 0 0 0-1H2.5A1.5 1.5 0 0 0 1 2.5z"/>
-                                </svg>
+                                <i class="bi bi-pencil-square"></i>
                             </button>
                             <b>in ${subgoal.file}</b>
                             ${linkBlock}
